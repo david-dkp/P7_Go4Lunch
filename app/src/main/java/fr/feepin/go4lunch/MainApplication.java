d0b87a3c5299a9f7b9c318212fa548acc0dde9e1
David DEKEUWER <david.dekeuwer@gmail.com>
2021-04-22 18:09:20 +0200

    create MainApplication for HiltAndroidApp

---
+++ app/src/main/java/fr/feepin/go4lunch/MainApplication.java
@@ -0,0 +1,9 @@
+package fr.feepin.go4lunch;
+
+import androidx.multidex.MultiDexApplication;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MainApplication extends MultiDexApplication {
+}
