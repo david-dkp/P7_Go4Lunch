a4d45e4fcf3abecac3abf291ac071de548572a04
David DEKEUWER <david.dekeuwer@gmail.com>
2021-04-26 20:22:14 +0200

    add constants

--- app/src/main/java/fr/feepin/go4lunch/Constants.java
+++ app/src/main/java/fr/feepin/go4lunch/Constants.java
@@ -3,4 +3,8 @@ package fr.feepin.go4lunch;
 public class Constants {
     public static final String MAPS_BASE_URL = "https://maps.googleapis.com";
     public static final float MAPS_START_ZOOM_LEVEL = 16;
+
+    public static final String NO_LOCATION_PERMISSION_MESSAGE = "No location permission";
+
+    public static final String USER_PREFS_NAME = "User preferences";
 }
