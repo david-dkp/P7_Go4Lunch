33495a875b2131bfbbe2e3ffa93a2d8a5362aa28
David DEKEUWER <david.dekeuwer@gmail.com>
2021-04-28 02:07:32 +0200

    fix list view search view text color

--- app/src/main/java/fr/feepin/go4lunch/ui/list/ListViewFragment.java
+++ app/src/main/java/fr/feepin/go4lunch/ui/list/ListViewFragment.java
@@ -1,14 +1,18 @@
 package fr.feepin.go4lunch.ui.list;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import fr.feepin.go4lunch.R;
@@ -32,5 +36,11 @@ public class ListViewFragment extends Fragment {
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.list_view_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        EditText editText = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        editText.setTextColor(Color.WHITE);
     }
 }
