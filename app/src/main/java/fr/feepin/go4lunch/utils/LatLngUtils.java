f4d5295d9f19ae680e384e473d56fe4134a52a40
David DEKEUWER <david.dekeuwer@gmail.com>
2021-04-29 18:05:40 +0200

    Add LatLngUtils, get LatLngBounds from radius and position

---
+++ app/src/main/java/fr/feepin/go4lunch/utils/LatLngUtils.java
@@ -0,0 +1,16 @@
+package fr.feepin.go4lunch.utils;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.maps.android.SphericalUtil;
+
+public class LatLngUtils {
+    public static LatLngBounds toBounds(LatLng center, double radiusInMeters) {
+        double distanceFromCenterToCorner = radiusInMeters * Math.sqrt(2.0);
+        LatLng southwestCorner =
+                SphericalUtil.computeOffset(center, distanceFromCenterToCorner, 225.0);
+        LatLng northeastCorner =
+                SphericalUtil.computeOffset(center, distanceFromCenterToCorner, 45.0);
+        return new LatLngBounds(southwestCorner, northeastCorner);
+    }
+}
