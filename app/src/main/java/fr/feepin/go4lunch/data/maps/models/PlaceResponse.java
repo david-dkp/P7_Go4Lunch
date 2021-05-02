322a044716723b5e7caca1828901c01be8d3a486
David DEKEUWER <david.dekeuwer@gmail.com>
2021-05-02 19:48:26 +0200

    Add more fields to PlaceResponse

--- app/src/main/java/fr/feepin/go4lunch/data/maps/models/PlaceResponse.java
+++ app/src/main/java/fr/feepin/go4lunch/data/maps/models/PlaceResponse.java
@@ -1,7 +1,5 @@
 package fr.feepin.go4lunch.data.maps.models;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.libraries.places.api.Places;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -17,14 +15,25 @@ public class PlaceResponse {
     private Geometry geometry;
 
     @Expose
+    private String name;
+
+    @Expose
     private List<Photo> photos;
 
     @Expose
     private String vicinity;
 
-    public PlaceResponse(String placeId, Geometry geometry) {
+    @SerializedName("opening_hours")
+    @Expose
+    private OpeningHours openingHours;
+
+    public PlaceResponse(String placeId, Geometry geometry, String name, List<Photo> photos, String vicinity, OpeningHours openingHours) {
         this.placeId = placeId;
         this.geometry = geometry;
+        this.name = name;
+        this.photos = photos;
+        this.vicinity = vicinity;
+        this.openingHours = openingHours;
     }
 
     public String getPlaceId() {
@@ -43,7 +52,65 @@ public class PlaceResponse {
         this.geometry = geometry;
     }
 
-    public class Geometry {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public String getVicinity() {
+        return vicinity;
+    }
+
+    public void setVicinity(String vicinity) {
+        this.vicinity = vicinity;
+    }
+
+    public OpeningHours getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(OpeningHours openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    public static class OpeningHours {
+
+        @SerializedName("open_now")
+        @Expose
+        private boolean openNow;
+
+        public OpeningHours(boolean openNow) {
+            this.openNow = openNow;
+        }
+
+        public boolean isOpenNow() {
+            return openNow;
+        }
+
+        public void setOpenNow(boolean openNow) {
+            this.openNow = openNow;
+        }
+
+        @Override
+        public String toString() {
+            return "OpeningHours{" +
+                    "openNow=" + openNow +
+                    '}';
+        }
+    }
+
+    public static class Geometry {
 
         @Expose
         private LatLng location;
@@ -62,37 +129,41 @@ public class PlaceResponse {
 
     }
 
-    public class LatLng {
+    public static class LatLng {
 
         @Expose
-        private float lat;
+        private double lat;
 
         @Expose
-        private float lng;
+        private double lng;
 
-        public LatLng(float lat, float lng) {
+        public LatLng(double lat, double lng) {
             this.lat = lat;
             this.lng = lng;
         }
 
-        public float getLat() {
+        public double getLat() {
             return lat;
         }
 
-        public void setLat(float lat) {
+        public void setLat(double lat) {
             this.lat = lat;
         }
 
-        public float getLng() {
+        public double getLng() {
             return lng;
         }
 
-        public void setLng(float lng) {
+        public void setLng(double lng) {
             this.lng = lng;
         }
 
         public com.google.android.gms.maps.model.LatLng toMapsLatLng() {
             return new com.google.android.gms.maps.model.LatLng(getLat(), getLng());
+        }
+
+        public static LatLng fromMapsLatLng(com.google.android.gms.maps.model.LatLng mapsLatLng) {
+            return new LatLng(mapsLatLng.latitude, mapsLatLng.longitude);
         }
     }
 
@@ -137,5 +208,17 @@ public class PlaceResponse {
         public void setReference(String reference) {
             this.reference = reference;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "PlaceResponse{" +
+                "placeId='" + placeId + '\'' +
+                ", geometry=" + geometry +
+                ", name='" + name + '\'' +
+                ", photos=" + photos +
+                ", vicinity='" + vicinity + '\'' +
+                ", openingHours=" + openingHours +
+                '}';
     }
 }
