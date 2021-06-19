cca8a5b7f3d22bbaeefc6ce096c3633052b5e97e
David DEKEUWER <david.dekeuwer@gmail.com>
2021-06-19 14:18:58 +0200

    Refactor naming

--- app/src/test/java/fr/feepin/go4lunch/sorts/ListItemStateSortMethodTest.java
+++ app/src/test/java/fr/feepin/go4lunch/sorts/ListItemStateSortMethodTest.java
@@ -8,70 +8,70 @@ import org.junit.runners.JUnit4;
 import java.util.Arrays;
 import java.util.List;
 
-import fr.feepin.go4lunch.ui.list.ListItemState;
+import fr.feepin.go4lunch.ui.list.ListViewState;
 
 @RunWith(JUnit4.class)
 public class ListItemStateSortMethodTest {
 
     @Test
     public void sortByDistance() {
-        List<ListItemState> listToSort = Arrays.asList(
-                new ListItemState(null, null, null, 324, 0, 0, null, null),
-                new ListItemState(null, null, null, 145, 0, 0, null, null),
-                new ListItemState(null, null, null, 678, 0, 0, null, null),
-                new ListItemState(null, null, null, 432, 0, 0, null, null)
+        List<ListViewState.ListItemState> listToSort = Arrays.asList(
+                new ListViewState.ListItemState(null, null, null, 324, 0, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 145, 0, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 678, 0, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 432, 0, 0, null, null)
         );
 
-        List<ListItemState> expectedSortedList = Arrays.asList(
-                new ListItemState(null, null, null, 145, 0, 0, null, null),
-                new ListItemState(null, null, null, 324, 0, 0, null, null),
-                new ListItemState(null, null, null, 432, 0, 0, null, null),
-                new ListItemState(null, null, null, 678, 0, 0, null, null)
+        List<ListViewState.ListItemState> expectedSortedList = Arrays.asList(
+                new ListViewState.ListItemState(null, null, null, 145, 0, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 324, 0, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 432, 0, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 678, 0, 0, null, null)
         );
 
-        listToSort.sort(ListItemState.DISTANCE_COMPARATOR);
+        listToSort.sort(ListViewState.ListItemState.DISTANCE_COMPARATOR);
 
         Assert.assertArrayEquals(listToSort.toArray(), expectedSortedList.toArray());
     }
 
     @Test
     public void sortByRating() {
-        List<ListItemState> listToSort = Arrays.asList(
-                new ListItemState(null, null, null, 0, 0, 2, null, null),
-                new ListItemState(null, null, null, 0, 0, 3, null, null),
-                new ListItemState(null, null, null, 0, 0, 1, null, null),
-                new ListItemState(null, null, null, 0, 0, 3, null, null)
+        List<ListViewState.ListItemState> listToSort = Arrays.asList(
+                new ListViewState.ListItemState(null, null, null, 0, 0, 2, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 0, 3, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 0, 1, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 0, 3, null, null)
         );
 
-        List<ListItemState> expectedSortedList = Arrays.asList(
-                new ListItemState(null, null, null, 0, 0, 3, null, null),
-                new ListItemState(null, null, null, 0, 0, 3, null, null),
-                new ListItemState(null, null, null, 0, 0, 2, null, null),
-                new ListItemState(null, null, null, 0, 0, 1, null, null)
+        List<ListViewState.ListItemState> expectedSortedList = Arrays.asList(
+                new ListViewState.ListItemState(null, null, null, 0, 0, 3, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 0, 3, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 0, 2, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 0, 1, null, null)
         );
 
-        listToSort.sort(ListItemState.RATING_COMPARATOR);
+        listToSort.sort(ListViewState.ListItemState.RATING_COMPARATOR);
 
         Assert.assertArrayEquals(listToSort.toArray(), expectedSortedList.toArray());
     }
 
     @Test
     public void sortByWorkmates() {
-        List<ListItemState> listToSort = Arrays.asList(
-                new ListItemState(null, null, null, 0, 15, 0, null, null),
-                new ListItemState(null, null, null, 0, 7, 0, null, null),
-                new ListItemState(null, null, null, 0, 25, 0, null, null),
-                new ListItemState(null, null, null, 0, 17, 0, null, null)
+        List<ListViewState.ListItemState> listToSort = Arrays.asList(
+                new ListViewState.ListItemState(null, null, null, 0, 15, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 7, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 25, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 17, 0, null, null)
         );
 
-        List<ListItemState> expectedSortedList = Arrays.asList(
-                new ListItemState(null, null, null, 0, 25, 0, null, null),
-                new ListItemState(null, null, null, 0, 17, 0, null, null),
-                new ListItemState(null, null, null, 0, 15, 0, null, null),
-                new ListItemState(null, null, null, 0, 7, 0, null, null)
+        List<ListViewState.ListItemState> expectedSortedList = Arrays.asList(
+                new ListViewState.ListItemState(null, null, null, 0, 25, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 17, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 15, 0, null, null),
+                new ListViewState.ListItemState(null, null, null, 0, 7, 0, null, null)
         );
 
-        listToSort.sort(ListItemState.WORKMATES_COMPARATOR);
+        listToSort.sort(ListViewState.ListItemState.WORKMATES_COMPARATOR);
 
         Assert.assertArrayEquals(listToSort.toArray(), expectedSortedList.toArray());
     }
